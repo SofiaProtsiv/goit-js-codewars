16972c52c817fd6493fb173f4dc725f324b949ad
Sofia Protsiv <prochivsofia@gmail.com>
2022-11-23 18:56:22 +0200

    fix: sort files

---
+++ 02-module-lesson-01/index.js
@@ -0,0 +1,44 @@
+// const months = [
+//   'січень',
+//   'лютий',
+//   'березень',
+//   'квітень',
+//   'травень',
+//   'червень',
+//   'липень',
+//   'серпень',
+//   'вересень',
+//   'жовтень',
+//   'листопад',
+//   'грудень',
+// ];
+// function arrayRandomElement(arr) {
+//   const random = Math.floor(Math.random() * arr.length);
+//   return arr[random];
+// }
+// const month = arrayRandomElement(months);
+
+// ///////////// reverse
+// const reversed = [];
+
+// for (let i = months.length - 1; i >= 0; i -= 1) {
+//   reversed.push(months[i]);
+// }
+
+// console.log(reversed);
+/////// fill
+// const arraySize = 3;
+// const valueToFill = 'a';
+// const fill = new Array(arraySize).fill(valueToFill);
+// console.log(fill);
+
+//////without
+// const data = [1, 2, 3, 1, 2];
+// const charToDelete = 2;
+// let filteredArray = [];
+// for (let i = 0; i < data.length; i += 1) {
+//   if (charToDelete !== data[i]) {
+//     filteredArray.push(data[i]);
+//   }
+// }
+// console.log(filteredArray);
